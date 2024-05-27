23227fd6ccdac1a55bbff9eb3ae18ccd434d1748
maskikuy <kproject305@gmail.com>
2024-05-27 15:39:53 +0700

    menuliskan void display

--- abstraksi_0060/abstraksi_0060.cpp
+++ abstraksi_0060/abstraksi_0060.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class Abstraksiklas {
+private: string x, y;
+
+public:
+
+	// method untuk mengisi nilai
+	// private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	// menampilkan nilai
+	void display() {
+		cout << "y=" << x << endl;
+	}
+};
